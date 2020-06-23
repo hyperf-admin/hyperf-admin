66dcd0cf3567c34a02be354851129e5fe50fb657
刀刀 <wangwenjie@innotechx.com>
2020-06-23 17:38:52 +0800

    perf: 优化 config-center

--- src/config-center/src/BootProcessListener.php
+++ src/config-center/src/BootProcessListener.php
@@ -45,9 +45,8 @@ class BootProcessListener implements ListenerInterface
         $this->log->info('tick begin');
         $interval = config('config_center.interval', 1);
         $this->load();
-        $that = $this;
-        Timer::tick($interval * 1000, function () use ($that) {
-            $that->load();
+        Timer::tick($interval * 1000, function () {
+            $this->load();
         });
     }
 
